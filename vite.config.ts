aab6c9c7040e9c3eeca9db8a9010c1dd8903d508
sushilsharma8 <sushilsharma8oct2001@gmail.com>
2026-06-26 17:59:47 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         background_color: '#fafaf9',
         display: 'standalone',
         scope: '/',
-        start_url: '/',
+        start_url: '/explore',
         icons: [
           { src: 'pwa-192x192.png', sizes: '192x192', type: 'image/png' },
           { src: 'pwa-512x512.png', sizes: '512x512', type: 'image/png' },
